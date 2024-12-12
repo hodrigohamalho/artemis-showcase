aa29e47de8ff5c56c89b6ff108a8dee7ef3ba29b
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2024-12-12 14:00:17 -0300

    adding queue list property to whitelist queues that must be replicated

--- apps/messaging-mirroring/src/main/java/sample/camel/SampleAutowiredAmqpRoute.java
+++ apps/messaging-mirroring/src/main/java/sample/camel/SampleAutowiredAmqpRoute.java
@@ -1,27 +1,42 @@
 package sample.camel;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
+@Configuration
 public class SampleAutowiredAmqpRoute extends RouteBuilder {
+
+    @Value("${queue.whitelist}")
+    private String whitelistQueues;
 
     @Override
     public void configure() throws Exception {
 
-        // SENDING MESSAGES TO IBM
-        from("timer:produce-ibm")
-            .id("producer-ibm")
-            .setBody(constant("Hello from Camel for IBM"))
-            .to("wmq:queue:SCIENCEQUEUE")
-            .log("Message sent to IBM Broker");
+        // Cria uma lista de filas permitidas
+        List<String> queues = Arrays.asList(whitelistQueues.split(","));
 
-        // CONSUME MESSAGES FROM IBM AND PRODUCE TO KAFKA
-        from("wmq:queue:SCIENCEQUEUE?receiveTimeout=10000")
-            .id("consumer-ibm")
-            .to("amqp:queue:SCIENCEQUEUE")
-            .log("Message consumed from IBM Broker and sent to AMQ Broker");
-        
+        for (String queue : queues) {
+            queue = queue.trim();
+
+            // Rota para consumir mensagens da fila do IBM MQ e enviar para o ActiveMQ Artemis
+            from("wmq:queue:" + queue)
+                .routeId("replicate-" + queue)
+                .log("Replicando mensagem da fila: " + queue + " no IBM para " + queue + " no Artemis")
+                .to("amqp:queue:" + queue);
+        }
+
+        // // SENDING MESSAGES TO IBM
+        // from("timer:produce-ibm")
+        //     .id("producer-ibm")
+        //     .setBody(constant("Hello from Camel for IBM"))
+        //     .to("wmq:queue:SCIENCEQUEUE")
+        //     .log("Message sent to IBM Broker");
         
     }
 
